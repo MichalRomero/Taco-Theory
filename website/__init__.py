4a889fe67fd38767b2da39e440a54b6b14033ea4
Michal Romero <michal.romero02@gmail.com>
2025-01-23 15:41:29 +0000

    Creating routes/views
    Added basic login, logout and sign-up routes.

--- website/__init__.py
+++ website/__init__.py
@@ -4,5 +4,13 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'NSJ/8shj4HD.f37h~jjk8#b7' #encrypts cookies and session data.
 
+    #importing blueprints
+    from .views import views 
+    from .auth import auth
+
+    #registering the blueprints
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
     return app
 
