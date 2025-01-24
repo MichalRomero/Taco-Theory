af73bcc3f034077eca025ec2aa66dbe83d6b3b46
Michal Romero <michal.romero02@gmail.com>
2025-01-24 18:00:22 +0000

    Add POST method and input validation to sign-up route
    Enabled POST and get method for the existing /sign-up and /login route route.
    Implemented input validation.
    Added placeholder logic for future database integration.

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,38 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    data = request.form
+    print(data)
     return render_template("login.html", text="Testing")
 
 @auth.route('/logout')
 def logout():
     return render_template("login.html")
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        
+        #Checks if user input is valid
+        if len(email) < 4:
+            pass
+        elif len(firstName) < 2:
+            pass
+        elif password1 != password2:
+            pass
+        elif password1 < 5:
+            pass
+        else:
+            #add user to database
+            pass
+
+        
+
     return render_template("sign_up.html")
