b2ce89aa08011680546848831235b177a7fecc91
Michal Romero <michal.romero02@gmail.com>
2025-01-26 14:03:58 +0000

    Set up SQLite database with SQLAlchemy
    Configured SQLite database using SQLAlchemy.
    Set SQLALCHEMY_DATABASE_URI to use database.db.
    Initialized the database with db.init_app(app).

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,19 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'NSJ/8shj4HD.f37h~jjk8#b7' #encrypts cookies and session data.
+
+    # Configures SQLAlchemy to use SQLite as the database. 
+    # The URI format 'sqlite:///{DB_NAME}' specifies a local SQLite database file named by the DB_NAME variable.
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+
+    # Initializes the database with the app
+    db.init_app(app)
 
     #importing blueprints
     from .views import views 
