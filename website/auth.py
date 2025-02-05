f410b7624813bdac9e02d484d188782014fa11cb
Michal Romero <michal.romero02@gmail.com>
2025-02-05 17:03:26 +0000

    Implement login validation and authentication
    This commit adds login validation by ensuring that both the email and password fields are filled. It then checks if the provided email exists in the database. If a matching user is found, it verifies the password using check_password_hash. Success or error messages are flashed based on the outcome, guiding the user accordingly. If the email does not exist or the password is incorrect, appropriate error messages are displayed.
    
    Additionally, in login.html, type='passsword' (3 's') was changed to type='password'

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,32 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    data = request.form
-    print(data)
-    return render_template("login.html", text="Testing")
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        if not email or not password:  # Ensure both fields are filled
+            flash('Please fill out both fields.', category='error')
+            return render_template("login.html")
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+
+    return render_template("login.html", boolean=True)
+    
 
 @auth.route('/logout')
 def logout():
@@ -22,9 +39,13 @@ def sign_up():
         first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
+
+        user = User.query.filter_by(email=email).first()
         
         #Checks if user input is valid, if yes then adds user to database
-        if len(email) < 4:
+        if user:
+            flash('Email already exists', category='error')
+        elif len(email) < 4:
             flash('Email must be greater than 3 characters.', category='error')
         elif len(first_name) < 2:
             flash('First name must be greater than 1 character.', category='error')
